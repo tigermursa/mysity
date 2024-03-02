915db36e720fd5ee5be015f3442a07fbb090fd27
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-02 21:08:50 +0600

    "active link activated"

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,5 +1,7 @@
+"use client"
 import Link from "next/link";
 import React from "react";
+import { usePathname } from 'next/navigation'
 import {
   FaHouseChimneyWindow,
   FaWhmcs,
@@ -15,45 +17,46 @@ import {
 } from "react-icons/fa6";
 
 const Sidebar = () => {
+    const pathname = usePathname()
   return (
     <aside className="bg-blue-950 p-4 lg:p-5 col-span-2 h-[100vh] sticky top-0 left-0 overflow-auto">
       <nav>
         <ul className="flex flex-col text-gray-500 font-bold font-mono e">
           <Link
-            className="  mb-3 hover:text-white flex items-center gap-2"
+            className={`mb-3 hover:text-white flex items-center gap-2 link ${pathname === '/' ? 'text-white' : 'text-gray-500'} `}  
             href={"/"}
           >
             <FaHouseChimneyWindow /> Home
           </Link>
           <Link
-            className="hover:text-white  mb-3 truncate flex items-center gap-2"
+           className={`mb-3 hover:text-white flex items-center gap-2 link ${pathname === '/dashboard/development' ? 'text-white' : 'text-gray-500'} `}
             href={"/dashboard/development"}
           >
             <FaWhmcs />
             Development
           </Link>
           <Link
-            className="hover:text-white  mb-3 truncate flex items-center gap-2"
+            className={`mb-3 hover:text-white flex items-center gap-2 link ${pathname === '/dashboard/ui' ? 'text-white' : 'text-gray-500'} `}
             href={"/dashboard/ui"}
           >
             <FaWandSparkles />
             UI/UX
           </Link>
           <Link
-            className="  hover:text-white  mb-3 truncate flex items-center gap-2"
+            className={`mb-3 hover:text-white flex items-center gap-2 link ${pathname === '/dashboard/learn' ? 'text-white' : 'text-gray-500'} `}
             href={"/dashboard/learn"}
           >
             <FaUserGraduate /> Learning
           </Link>
           <Link
-            className="  hover:text-white  mb-3 truncate flex items-center gap-2"
+           className={`mb-3 hover:text-white flex items-center gap-2 link ${pathname === '/dashboard/ai' ? 'text-white' : 'text-gray-500'} `}
             href={"/dashboard/ai"}
           >
             <FaGithubAlt />
             AI
           </Link>
           <Link
-            className=" hover:text-white  mb-3 truncate flex items-center gap-2"
+           className={`mb-3 hover:text-white flex items-center gap-2 link ${pathname === '/dashboard/github' ? 'text-white' : 'text-gray-500'} `}
             href={"/dashboard/github"}
           >
             <FaSquareGithub />
@@ -61,33 +64,33 @@ const Sidebar = () => {
           </Link>
 
           <Link
-            className=" hover:text-white  mb-3 truncate flex items-center gap-2"
+            className={`mb-3 hover:text-white flex items-center gap-2 link ${pathname === '/dashboard/news' ? 'text-white' : 'text-gray-500'} `}
             href={"/dashboard/news"}
           >
             <FaGlobe /> News
           </Link>
           <Link
-            className=" hover:text-white  mb-3 truncate flex items-center gap-2"
+           className={`mb-3 hover:text-white flex items-center gap-2 link ${pathname === '/dashboard/google' ? 'text-white' : 'text-gray-500'} `}
             href={"/dashboard/google"}
           >
             <FaGoogle />
             Google
           </Link>
           <Link
-            className="  hover:text-white  mb-3 truncate flex items-center gap-2"
+            className={`mb-3 hover:text-white flex items-center gap-2 link ${pathname === '/dashboard/resume' ? 'text-white' : 'text-gray-500'} `}
             href={"/dashboard/resume"}
           >
             <FaIdCard />
             Resume
           </Link>
           <Link
-            className=" hover:text-white  mb-3 truncate flex items-center gap-2 "
+            className={`mb-3 hover:text-white flex items-center gap-2 link ${pathname === '/dashboard/search' ? 'text-white' : 'text-gray-500'} `}
             href={"/dashboard/search"}
           >
             <FaSistrix /> Search Engines
           </Link>
           <Link
-            className=" hover:text-white  mb-3 truncate flex items-center gap-2 "
+            className={`mb-3 hover:text-white flex items-center gap-2 link ${pathname === '/dashboard/others' ? 'text-white' : 'text-gray-500'} `}
             href={"/dashboard/others"}
           >
             <FaTag /> Others
--- src/app/loading.js
+++ src/app/loading.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const LoadingPage = () => {
-    return (
-        <div>
-            <h1 className="text-8xl font-bold text-red-700 flex justify-center items-center h-screen bg-black">
+  return (
+    <div className="h-screen flex justify-center items-center bg-black">
+      <h1 className="text-8xl font-bold bg-gradient-to-r from-cyan-500 to-pink-500 text-transparent bg-clip-text">
         Loading...
         <span className="animated-emoji">🔍</span>
       </h1>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default LoadingPage;
