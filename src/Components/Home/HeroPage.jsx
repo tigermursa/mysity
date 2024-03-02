88eaadf53d863a0c5a003b7fd3b360571a28c50b
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-03 00:32:32 +0600

    "going to add mongo data base"

--- src/Components/Home/HeroPage.jsx
+++ src/Components/Home/HeroPage.jsx
@@ -5,7 +5,7 @@ const HeroPage = () => {
   return (
     <div className="bg-slate-950 h-screen flex flex-col text-white justify-center items-center">
       <h1 className="font-mono text-7xl">
-        Welcome to My<span className="text-cyan-400">s</span>ity 😊
+        Welcome to My<span className="text-cyan-400 font-serif">s</span>ity 😊
       </h1>
       <Link href={"/dashboard"}>
         <button className="mt-10 p-3 bg bg-gradient-to-r from-cyan-400 to-pink-600 rounded-xl font-semibold text-lg">
